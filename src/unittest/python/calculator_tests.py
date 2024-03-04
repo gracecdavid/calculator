5f51a3d394cb4edf7d54ef53a3b4652a8cc8b6b9
Grace David <gracecandy.david@gmail.com>
2024-03-04 22:25:55 +0530

    Commiting Source Files

---
+++ src/unittest/python/calculator_tests.py
@@ -0,0 +1,13 @@
+# calculator_tests.py
+import unittest
+from calculator import Calculator
+
+class CalculatorTest(unittest.TestCase):
+    def test_addition(self):
+        calculator = Calculator()
+        result = calculator.add(2,3)
+        self.assertEqual(result, 5)
+        
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ target/dist/pyb-1.0.dev0/calculator.py
@@ -0,0 +1,12 @@
+# calculator.py
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+    
+#if __name__== "__main__":
+ #   calculator = Calculator()
+  #  result = calculator.add(5,3)
+   # print("Addition: {result}")
+
+
