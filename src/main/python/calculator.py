930952ceb4b5a3c7aa06cac76d1050c56caaa2af
Grace David <gracecandy.david@gmail.com>
2024-03-09 13:06:37 +0530

    Updated Code

--- src/main/python/calculator.py
+++ src/main/python/calculator.py
@@ -1,12 +1,24 @@
 # calculator.py
-
 class Calculator:
     def add(self, a, b):
         return a + b
-    
-#if __name__== "__main__":
- #   calculator = Calculator()
-  #  result = calculator.add(5,3)
-   # print("Addition: {result}")
+    def sub(self, a, b):
+        return a - b
+def main():
+    calc = Calculator()
+    print("Enter two numbers:")
+    num1 = float(input("First number: "))
+    num2 = float(input("Second number: "))
+    print("1. Addition\n2. Subtraction")
+    choice = input("Enter your choice (1 or 2): ")
+    if choice == '1':
+        result = calc.add(num1, num2)
+        print("Result:", result)
+    elif choice == '2':
+        result = calc.sub(num1, num2)
+        print("Result:", result)
+    else:
+        print("Invalid choice!")
 
-
+if __name__ == "__main__":
+    main()
--- src/unittest/python/calculator_tests.py
+++ src/unittest/python/calculator_tests.py
@@ -1,13 +1,10 @@
 # calculator_tests.py
 import unittest
 from calculator import Calculator
-
 class CalculatorTest(unittest.TestCase):
     def test_addition(self):
         calculator = Calculator()
         result = calculator.add(2,3)
-        self.assertEqual(result, 5)
-        
-
+        self.assertEqual(result, 5)    
 if __name__ == '__main__':
     unittest.main()
